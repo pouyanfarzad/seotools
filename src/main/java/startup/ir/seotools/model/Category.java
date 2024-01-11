6fee692f81a56020045d8836fef9312495aa7bd7
pouyanfarzad <pouyanfarzad@gmail.com>
2024-01-11 10:48:49 +0330

    Config and Connect to database named seotools

--- src/main/java/startup/ir/seotools/model/Category.java
+++ src/main/java/startup/ir/seotools/model/Category.java
@@ -1,12 +1,17 @@
 package startup.ir.seotools.model;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Category {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int weight;

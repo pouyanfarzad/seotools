c33cef9578dad93f2596e41a514f5364bb1324d4
pouyanfarzad <pouyanfarzad@gmail.com>
2024-01-17 11:56:49 +0330

    adding News Model

--- src/main/java/startup/ir/seotools/model/Category.java
+++ src/main/java/startup/ir/seotools/model/Category.java
@@ -1,10 +1,8 @@
 package startup.ir.seotools.model;
 
 import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -16,14 +14,17 @@ public class Category {
     }
 
     public Category() {
-
     }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int weight;
     private String iconPath;
+
+    @OneToOne(mappedBy = "category")
+    private News news;
 
 
 }
